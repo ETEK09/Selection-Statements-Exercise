dc01d6e6a10ebb4e43c9d228501d0a4389684185
Eric Caro <c.aroericm@gmail.com>
2023-06-21 14:38:10 -0500

    Exercise 1

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+
+            var favNumber = r.Next(15);
+            Console.WriteLine("Try to guess my favorite number");
+            var userInput = int.Parse(Console.ReadLine());
+
+
+            if (userInput == 7)
+            {
+                Console.WriteLine("This is your number");
+            }
+            else if (userInput >= 7)
+            {
+                Console.WriteLine("Too high");
+                
+            }
+            else if (userInput <= 7)
+            {
+                Console.WriteLine("Too low"); 
+            }
+            else
+            {
+                Console.WriteLine("Can't use this number");
+               
+            }
         }
     }
 }
